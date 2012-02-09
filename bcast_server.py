1c887d5c5342b574d8f495bccc769175cdd0f148
Wiliam Souza <wiliam@mandriva.com>
2012-02-09 14:17:36 -0200

    Chnaged debug level

--- bcast_server.py
+++ bcast_server.py
@@ -6,7 +6,7 @@ from broadcast import BroadcastServer
 
 if __name__ == '__main__':
     time.sleep(2)
-    multiprocessing.log_to_stderr(logging.INFO)
+    multiprocessing.log_to_stderr(logging.DEBUG)
     message = 'bcastserver'
     datagram_size = len(message)
     server = BroadcastServer('255.255.255.255', 65535, message)
