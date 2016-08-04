1a93e71f62d6750af8a0c7b669c613a53540d3b6
Wiliam Souza <wiliamsouza83@gmail.com>
2016-08-03 22:22:17 -0300

    Updated ubuntu bootstrap script

--- ubuntu-bootstrap.sh
+++ ubuntu-bootstrap.sh
@@ -4,82 +4,63 @@ DEVEL_DIR=$HOME/devel
 SOURCE_DIR=$HOME/source
 LOCAL_BIN=$HOME/.local/bin
 DOT_REPO_DIR=$SOURCE_DIR/dot
+PYTHON_VERION=3.5.1
+RUBY_VERSION=2.3.1
+GO_VERSION=1.6.3
 
 mkdir -p $DEVEL_DIR
 mkdir -p $SOURCE_DIR
 mkdir -p $LOCAL_BIN
 
 sudo apt-get install -y openssh-server git gnome-tweak-tool vim tmux screen \
-    python-flake8 curl tree python-pip virtualenvwrapper virtualenv-clone \
-    python-virtualenv make build-essential libssl-dev zlib1g-dev libbz2-dev \
-    libreadline-dev libsqlite3-dev wget llvm python-jedi python-dev ipython \
-    libyaml-dev bzr mercurial
+    curl tree apt-transport-https ca-certificates  ack-grep \
+    make build-essential libssl-dev zlib1g-dev libbz2-dev \
+    libreadline-dev libsqlite3-dev wget python-dev ipython \
+    libyaml-dev bzr mercurial linux-image-extra-$(uname -r)
 
 echo "Installing docker-compose"
-curl -L https://github.com/docker/compose/releases/download/1.3.1/docker-compose-`uname -s`-`uname -m` > $LOCAL_BIN/docker-compose
-chmod +x $LOCAL_BIN/docker-compose
+if [ ! -f $LOCAL_BIN/docker-compose ]; then
+    curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > $LOCAL_BIN/docker-compose
+    chmod +x $LOCAL_BIN/docker-compose
+fi
 
 echo "Installing docker"
-wget -qO- https://get.docker.com/ | sh
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
+sudo apt-get update
+sudo apt-get purge -y lxc-docker
+sudo apt-get install -y docker-engine
+sudo systemctl enable docker
 sudo usermod --append --groups docker $USER
 echo "Your user was added to docker group."
 echo "To this change take place you need to login again."
 
-
 if [ ! -d $DOT_REPO_DIR ]; then
     git clone https://github.com/wiliamsouza/dot.git $DOT_REPO_DIR
-    pushd $DOT_REPO_DIR
+    cd $DOT_REPO_DIR
     bash install.sh
-    popd
+    cd -
 fi
 
-source $HOME/.bashrc
-
 echo "Installing vim plugins"
-mkdir -p $HOME/.vim/autoload
 mkdir -p $HOME/.vim/bundle
-
-curl -LSso $HOME/.vim/autoload/pathogen.vim \
-    https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
-
-git clone https://github.com/honza/dockerfile.vim.git \
-    $HOME/.vim/bundle/dockerfile.vim
-
-git clone https://github.com/nvie/vim-flake8 \
-     $HOME/.vim/bundle/vim-flake8
-
-git clone https://github.com/plasticboy/vim-markdown.git \
-    $HOME/.vim/bundle/vim-markdown
-
-git clone git://github.com/tpope/vim-fugitive.git \
-    $HOME/.vim/bundle/vim-fugitive
-
-git clone https://github.com/scrooloose/nerdtree.git \
-    $HOME/.vim/bundle/nerdtree
-
-git clone --recursive https://github.com/davidhalter/jedi-vim.git \
-    $HOME/.vim/bundle/jedi-vim
-
-git clone git://github.com/altercation/vim-colors-solarized.git \
-    $HOME/.vim/bundle/vim-colors-solarized
-
-git clone https://github.com/kien/ctrlp.vim.git \
-    $HOME/.vim/bundle/ctrlp.vim.git
-
-echo "Intalling rbenv"
-git clone https://github.com/sstephenson/rbenv.git $HOME/.rbenv
-git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 
 echo "Installing pyenv"
 git clone git://github.com/yyuu/pyenv.git $HOME/.pyenv
 git clone git://github.com/concordusapps/pyenv-implict.git $HOME/.pyenv/plugins/pyenv-implict
-pyenv install 2.7.6
-pyenv install 3.4.1
-pyenv install pypy-2.3
+export PYENV_ROOT=$HOME/.pyenv
+export PATH=$PYENV_ROOT/bin:$PATH
+eval "$(pyenv init -)"
+pyenv install $PYTHON_VERION
 pyenv rehash
+pyenv global $PYTHON_VERION
+wget --quiet -O - https://bootstrap.pypa.io/get-pip.py | python -
+pip install virtualenv
+pip install virtualenvwrapper
 
 echo "Installing powerline"
-pip install --user git+git://github.com/Lokaltog/powerline
+pip install git+git://github.com/Lokaltog/powerline
 mkdir -p $HOME/.fonts
 mkdir -p $HOME/.fonts.conf.d
 curl -LSso $HOME/.fonts/PowerlineSymbols.otf \
@@ -88,8 +69,31 @@ curl -LSso $HOME/.fonts.conf.d/10-powerline-symbols.conf \
     https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf
 fc-cache -vf $HOME/.fonts
 
-echo "Installing git-flow-completion"
-mkdir -p $HOME/.bash_completion.d
-curl -LSso $HOME/.bash_completion.d/git-flow-completion.bash \
-    https://raw.githubusercontent.com/bobthecow/git-flow-completion/master/git-flow-completion.bash
-source $HOME/.bash_completion.d/git-flow-completion.bash
+echo "Installing terraform"
+if [ ! -f $HOME/.local/bin/terraform ]; then
+    wget --quiet -O $HOME/.local/bin/terraform.zip https://releases.hashicorp.com/terraform/0.7.0/terraform_0.7.0_linux_amd64.zip
+    cd $HOME/.local/bin/
+    unzip terraform.zip
+    cd -
+fi
+
+echo "Installing heroku toolbelt"
+wget --quiet -O - https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
+echo "Installing golang"
+wget --quiet -O - https://godeb.s3.amazonaws.com/godeb-amd64.tar.gz | tar zxvf - -C $HOME/.local/bin/
+godeb install $tO_VERSION
+
+echo "Intalling rbenv"
+git clone https://github.com/sstephenson/rbenv.git $HOME/.rbenv
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+export RBENV_ROOT=$HOME/.rbenv
+export PATH=$RBENV_ROOT/bin:$PATH
+eval "$(rbenv init -)"
+export PATH=$HOME/.rbenv/versions/$RUBY_VERSION/bin/:$PATH
+rbenv install $RUBY_VERSION
+rbenv rehash
+rbenv global $RUBY_VERSION
+
+echo "Installing terraforming"
+gem install terraforming
