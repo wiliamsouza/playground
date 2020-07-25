f033e3611d72e419d7f1c1b1c41354b2d4870efd
Wiliam Souza <wiliamsouza83@gmail.com>
2020-07-25 12:31:09 -0300

    Add silver search and hub to install

--- macos-bootstrap.sh
+++ macos-bootstrap.sh
@@ -24,7 +24,7 @@ fi
 echo "Install brew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-brew install wget neovim tree
+brew install wget neovim tree the_silver_searcher hub
 
 echo "Installing pyenv"
 git clone git://github.com/yyuu/pyenv.git $HOME/.pyenv
