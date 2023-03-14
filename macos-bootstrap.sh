fd5eb326d14589c5d31c7a39075edb89a6a32655
Wiliam Souza <wiliamsouza83@gmail.com>
2023-03-14 10:11:29 -0300

    fix pyenv install

--- macos-bootstrap.sh
+++ macos-bootstrap.sh
@@ -24,7 +24,7 @@ echo "Install brew"
 brew install wget neovim tree the_silver_searcher jq
 
 echo "Installing pyenv"
-git clone https://github.com/rbenv/rbenv.git $HOME/.pyenv
+git clone https://github.com/pyenv/pyenv.git $HOME/.pyenv
 git clone https://github.com/concordusapps/pyenv-implict.git $HOME/.pyenv/plugins/pyenv-implict
 export PYENV_ROOT=$HOME/.pyenv
 export PATH=$PYENV_ROOT/bin:$PATH
